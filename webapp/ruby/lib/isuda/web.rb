2609a651d9a92838664a56039986f4b47d6cc647
michiomochi <michikawa.masayoshi@gmail.com>
2016-09-18 16:41:01 +0900

    Fix

--- webapp/ruby/lib/isuda/web.rb
+++ webapp/ruby/lib/isuda/web.rb
@@ -196,7 +196,7 @@ module Isuda
       isutar_db.xquery('TRUNCATE star')
 
       db.xquery(%| select id, password, salt from user |).each do |user|
-        redis.set("user_#{user['name']}", { id: user['id'], salt: user['salt'], password: user['password'] }.to_json)
+        redis.set("user_#{user[:name]}", { id: user[:id], salt: user[:salt], password: user[:password] }.to_json)
       end
 
       content_type :json
